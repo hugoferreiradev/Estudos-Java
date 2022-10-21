6a78092ab1821b241d4da6ab9e11c58530d2d77e
Hugo <hugodacruzferreira1@gmail.com>
2022-10-21 14:04:12 -0300

    Primeiro exercício

---
+++ IdiomaSistema/src/IdiomaSistema.java
@@ -0,0 +1,14 @@
+
+import java.util.Locale;
+
+public class IdiomaSistema {
+
+    public static void main(String[] args) {
+        Locale loc = Locale.getDefault();
+        System.out.println("O idioma do sistema é: ");
+        System.out.println(loc.getDisplayLanguage());
+        System.out.println(loc.getLanguage());
+    }
+}
+
+// Pegar o idioma do sistema.
